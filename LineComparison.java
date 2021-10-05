a97ff91a672a96daeb89f5a2b0e76da6169c322a
pooj1277 <pooja.sd3@gmail.com>
2021-10-04 18:35:38 -0700

    [Added]: Welcome message for Line Comparison Program using OOPS concepts

---
+++ LineComparison.java
@@ -0,0 +1,12 @@
+/**
+ * Welcome to Line Comparison Computation Program
+ *
+ */
+package com.Bridgelabz.LineComparisonOOPS;
+public class LineComparison 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Line Comparison Computation Program" );
+    }
+}
